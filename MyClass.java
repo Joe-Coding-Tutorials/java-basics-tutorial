fdc916a2e431fda16843d9deab840145f6f33d29
Joe Bennouna <yo.m.hsm3@gmail.com>
2020-11-04 15:01:14 +0200

    initial

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+  int x = 5;    // Create class attribute
+
+  // Create a class constructor for the MyClass class
+  public MyClass() {
+    x = 5;  // Set the initial value for the class attribute x
+  }
+
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass(); // Create an object of class MyClass (This will call the constructor)
+    System.out.println(myObj.x); // Print the value of x
+  }
+}
